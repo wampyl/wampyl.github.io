640cb8c71dd334be72a0f0e9403b3efd37d1367e
wamp <wampyl@foxmail.com>
2021-02-02 00:04:50 +0800

    put

---
+++ vue-evernote-client/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="login">
+    <h1>{{ msg }}</h1>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            msg: 'This is login page'
+        }
+    }
+
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
